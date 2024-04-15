02532a7af51bcc6cc48b2c7395f73221a3f78d90
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-15 16:47:27 -0300

    projeto react

---
+++ uc3/aula01/testando_react/first-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <a href="#">
+        <img className='logo' src={reactLogo} alt="Logo do React" />
+      </a>
+      <h1>Meu primeiro Projeto <span>React</span></h1>
+      <p>Esse é o meu primeiro projeto <span>React</span>, onde estou fazendo sozinho após a aula da professora Débora.</p>
+      <a href="https://react.dev/" target='_blank'>Saiba mais</a>
+    </>
+  )
+}
+
+export default App
