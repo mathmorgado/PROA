ac27d45016ad1cc72987a9a9b803b1e585325a08
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 23:57:50 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/for/9-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/for/9-exercicio/script.js
@@ -1,14 +1,14 @@
 let idade;
 let media;
 let acumulador = 0;
-let contador = 0;
 
-while(contador < 5)
+
+for (let contador = 0; contador < 5; contador++)
 {
     idade = parseInt(prompt("Qual sua idade: "));
 
     acumulador += idade;
-    contador++
+   
 }
 
 media = acumulador / 5;
