57006f136abf485fd18b40066bef01a4a9212f0a
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 23:57:28 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/for/14-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/for/14-exercicio/script.js
@@ -1,11 +1,10 @@
 let numero = parseInt(prompt("Digite um número: "));
 let fatorial = 1;
-let contador = numero;
 
-while(contador > 1)
+
+for (let contador = numero; contador > 1; contador -= 1)
 {
     fatorial *= contador;
-    contador -= 1;
 }
 
 alert(`${numero}! = ${fatorial}`)
