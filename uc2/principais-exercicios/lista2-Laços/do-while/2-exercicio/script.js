e8b7d35ef4b7843e5c465f938ed0dd26424d5a3d
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 22:20:39 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/do-while/2-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/do-while/2-exercicio/script.js
@@ -5,10 +5,11 @@ Escreva um programa que calcule a soma dos números de 1 a 100 usando o loop "wh
 let num = 1
 let acumulador = 0
 
-while (num <= 100)
+do
 {
     acumulador += num
     num++
 }
+while (num <= 100)
 
 document.write("Soma dos números de 0 a 100: " + acumulador)
