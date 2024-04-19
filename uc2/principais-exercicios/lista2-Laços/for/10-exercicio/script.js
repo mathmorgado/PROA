2114d5079c2f35a031d4db6d07ccc7e921440ba9
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 23:57:18 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/for/10-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/for/10-exercicio/script.js
@@ -1,15 +1,12 @@
 let fib1 = 0;
 let fib2 = 1;
-let contador = 0;
 let intermediador;
 
-while(contador < 20)
+for (let contador = 0; contador < 20; contador++)
 {
     document.write(`${fib1}, `);
 
     intermediador = fib1
     fib1 = fib2;
     fib2 = intermediador + fib2;
-
-    contador++;
 }
