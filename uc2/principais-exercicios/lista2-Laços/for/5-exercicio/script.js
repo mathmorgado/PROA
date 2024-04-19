91670027cef181f7db67ad655a361fb54174ed6a
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 23:57:41 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/for/5-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/for/5-exercicio/script.js
@@ -1,10 +1,8 @@
 /* Escreva um programa que leia um número inteiro e exiba a tabuada desse número até o décimo valor usando o loop "while". */
 
 let numeroDaTabuada = parseInt(prompt("Digite o número da tabuada que deseja ver: "))
-let contador = 1
 
-while (contador <= 10)
+for (let contador = 1; contador <= 10; contador++)
 {
     document.write("<br>" + numeroDaTabuada + " x " + contador + " = " + (numeroDaTabuada*contador))
-    contador++
 }
