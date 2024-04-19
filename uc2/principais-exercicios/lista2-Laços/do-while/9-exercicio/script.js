79ed021f9a1e88c47e2f4d55a17fedf99a20222d
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 22:25:37 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/do-while/9-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/do-while/9-exercicio/script.js
@@ -3,13 +3,14 @@ let media;
 let acumulador = 0;
 let contador = 0;
 
-while(contador < 5)
+do
 {
     idade = parseInt(prompt("Qual sua idade: "));
 
     acumulador += idade;
     contador++
 }
+while(contador < 5)
 
 media = acumulador / 5;
 alert(`A média das idades é ${media}`);
