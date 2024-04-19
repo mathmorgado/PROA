9e68c3a9722a73d9599f9f18a98674fdfcf86f80
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 23:57:36 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/for/3-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/for/3-exercicio/script.js
@@ -1,13 +1,11 @@
 /*Crie um programa que exiba os números pares de 1 a 50 usando o loop "while".*/
 
-let num = 0
 
-while (num <= 50)
+
+for (let num = 0; num <= 50; num++)
 {
     if (num % 2 == 0)
     {
         document.write("<br>" + num)
     }
-    num++
-
 }
