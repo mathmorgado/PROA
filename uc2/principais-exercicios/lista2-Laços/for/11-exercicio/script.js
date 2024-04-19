37b0edb4fc4658c779e582271c277cf37056892f
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-18 23:57:21 -0300

    Update script.js

--- uc2/principais-exercicios/lista2-Laços/for/11-exercicio/script.js
+++ uc2/principais-exercicios/lista2-Laços/for/11-exercicio/script.js
@@ -1,15 +1,13 @@
 let numero = parseInt(prompt("Digite um número"));
 
-let contador = 2;
 let somaPares = 0;
 
-while(contador < numero)
+for (let contador = 2; contador < numero; contador++)
 {
     if (contador % 2 == 0)
     {
         somaPares += contador;
     };
-    contador++;
 };
 
 alert(`A soma de todos os pares entre 1 e ${numero} é: ${somaPares}`);
