9c6c973fc413edfa178fa49fb34a09ed01195c03
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-24 12:12:03 -0300

    Ex22-java

--- uc5/ExerciciosJava/src/exercicios/lista1/Exercicio_22.java
+++ uc5/ExerciciosJava/src/exercicios/lista1/Exercicio_22.java
@@ -1,7 +1,20 @@
 package exercicios.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio_22 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int contador = 0;
 
+        for (int i = 0; i < 20; i++) {
+            System.out.print("Digite um valor: ");
+            int valor = input.nextInt();
+            if (valor >= 0 && valor <= 100) {
+                contador++;
+            }
+        }
+
+        System.out.println("Você digitou " + contador + " numeros entre 0 e 100!");
     }
 }
