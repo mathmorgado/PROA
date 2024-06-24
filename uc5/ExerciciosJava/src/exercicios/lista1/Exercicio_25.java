eed7fc4ba05cbb094d6c42fe6b2f6a40f0fd7bae
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-24 12:12:26 -0300

    Ex25-java

--- uc5/ExerciciosJava/src/exercicios/lista1/Exercicio_25.java
+++ uc5/ExerciciosJava/src/exercicios/lista1/Exercicio_25.java
@@ -1,7 +1,22 @@
 package exercicios.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio_25 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int acumulador = 0;
+        int valor = 0;
 
+        do
+        {
+            System.out.print("Digite um valor: ");
+            valor = input.nextInt();
+            if (valor > 0) {
+                acumulador += valor;
+            }
+        } while (valor >= 0);
+
+        System.out.println("A soma de todos os numeros digitados e: " + acumulador);
     }
 }
