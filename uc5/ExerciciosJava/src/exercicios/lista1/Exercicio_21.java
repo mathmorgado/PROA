213ee4fd15bf0f75b3fa0750f105e806caaf6569
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-24 12:11:54 -0300

    Ex21-java

--- uc5/ExerciciosJava/src/exercicios/lista1/Exercicio_21.java
+++ uc5/ExerciciosJava/src/exercicios/lista1/Exercicio_21.java
@@ -1,7 +1,17 @@
 package exercicios.lista1;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Exercicio_21 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Valor da compra: ");
+        float valorCompra = input.nextFloat();
+
+        float prestacao = valorCompra / 5;
+
+        System.out.println("A compra pode ser feita em 5 prestacoes de: R$" + prestacao);
     }
 }
