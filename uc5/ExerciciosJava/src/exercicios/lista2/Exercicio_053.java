4c404d55959222032ab7cb5bf19539b5a61cf4c1
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-26 23:02:26 -0300

    Update Exercicio_053.java

--- uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_053.java
+++ uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_053.java
@@ -2,6 +2,7 @@ package exercicios.lista2;
 
 public class Exercicio_053 {
     public static void main(String[] args) {
+
         for (int i = 1; i <= 50; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
