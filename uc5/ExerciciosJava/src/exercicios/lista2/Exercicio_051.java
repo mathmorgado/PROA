7c6d229982c41a79a2215ca6a073c3e335bc4437
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-26 23:02:15 -0300

    Update Exercicio_051.java

--- uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_051.java
+++ uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_051.java
@@ -1,6 +1,7 @@
 package exercicios.lista2;
 
 public class Exercicio_051 {
+
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
