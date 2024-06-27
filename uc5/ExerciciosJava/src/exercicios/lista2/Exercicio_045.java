f78c17945f3fdc4bbb87eab9597bef3d3b98f31c
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-26 22:56:55 -0300

    Update Exercicio_045.java

--- uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_045.java
+++ uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_045.java
@@ -3,7 +3,6 @@ package exercicios.lista2;
 public class Exercicio_045 {
     public static void main(String[] args) {
         int i = 0, a = 0, b = 1;
-
         while (i < 20) {
             System.out.print(a + " ");
 
