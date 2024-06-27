90146e5e7ebb6e871f0e87f53359d47fb7c21b8b
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-26 22:56:01 -0300

    Update Exercicio_030.java

--- uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_030.java
+++ uc5/ExerciciosJava/src/exercicios/lista2/Exercicio_030.java
@@ -13,7 +13,6 @@ public class Exercicio_030 {
         double valorTotal = valorMacas * numMacas;
 
         System.out.printf("Valor total a ser pago: R$ %.2f\n", valorTotal);
-
         input.close();
     }
 
